2db4d0f06512772e9d977f2a4840fb9d51845ab3
rahul agrawal <rahulagrawal4918>
2023-03-06 01:17:17 +0530

    gf

--- src/Component/Admin/AdminHome.jsx
+++ src/Component/Admin/AdminHome.jsx
@@ -1,71 +1,64 @@
-import React, { useState, useEffect } from 'react'
-
-import { useSelector, useDispatch } from 'react-redux';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
 import LeftNav from './LeftNav'
-import { getUser } from "../../Store/ActionCreators/UserActionCreators"
-import { Link } from 'react-router-dom';
 
-export default function AdminHome() {
-    var [user, setuser] = useState({})
-    var users = useSelector((state) => state.UserStateData)
-    var dispatch = useDispatch()
-    function getAPIData() {
-        dispatch(getUser())
-        var d = users.find((item) => item.id === Number(localStorage.getItem("userid")))
-        if (d) {
-            setuser(d)
-        }
-    }
-    useEffect(() => {
-        getAPIData()
-    }, [users.length])
-  return (
-    <>
-        <div className="contain-fluid my-5">
-            <div className="row">
-                <div className="col-lg-2 col-12">
-                    <LeftNav/>
-                </div>
-                <div className="col-lg-10 col-12">
-                    <div className="row">
-                        <div className="col-md-5">
-                           {
-                            user.pic?
-                            <img src={`/assets/productimages/${user.pic}`} width="100%" height="420px" alt="" />:
-                            <img src="assets/images/noimage.png" width="100%" height="420px" alt="" />
-                           }
-                        </div>
-                        <div className="col-md-7">
-                            <h5 className='bg-secondary text-light text-center'>Admin Home</h5>
-                            <div className='d-flex'>
-                                <div className='border  p-3 w-50'>Name</div>
-                                <div className='border  p-3 w-50'>{user.name}</div>
-                            </div>
-                            <div className='d-flex'>
-                                <div className='border  p-3 w-50'>User Name</div>
-                                <div className='border  p-3 w-50'>{user.username}</div>
-                            </div>
-                            <div className='d-flex'>
-                                <div className='border  p-3 w-50'>Email</div>
-                                <div className='border  p-3 w-50'>{user.email}</div>
-                            </div>
-                            <div className='d-flex'>
-                                <div className='border  p-3 w-50'>Phone</div>
-                                <div className='border  p-3 w-50'>{user.phone}</div>
-                            </div>
-                            <div className='d-flex'>
-                                <div className='border  p-3 w-50'>Role</div>
-                                <div className='border  p-3 w-50'>{user.role}</div>
-                            </div>
-                            <div className='mt-3'>
-                                <Link to="/update-profile" className='btn btn-secondary w-100'>Update Profile</Link>
-                            </div>
-                        </div>
+
+export default function Admin() {
+    return (
+        <>
+            <div className="contain-fluid my-5 overflow-hidden">
+                <div className="row">
+                    <div className="col-lg-2 col-12">
+                        <LeftNav/>
                     </div>
+                    <div className="col-lg-10 col-12">
+                        <div className="row">
+                            <div className=" col-md-5 mt-2 w-100 ">
+                                <img src="assets/images/bg_1.jpg" height="400px" alt="img" />
+                            </div>
+                            <div className=" col-md-7">
+                                <div className=" col-lg-12">
+                                    <h4 className='text-center bg-secondary text-light  '>Admin Profile</h4>
+                                    <div className="admin-table d-flex ">
+                                        <div className='border py-3 pl-2 w-50 '>Name</div>
+                                        <div className='border py-3 pl-2 w-50 '>Rahul</div>
+                                    </div>
+                                    <div className="admin-table d-flex ">
+                                        <div className='border py-3 pl-2 w-50 '>User ID</div>
+                                        <div className='border py-3 pl-2 w-50 '>rahul_agrawal7773</div>
+                                    </div>
+                                    <div className="admin-table d-flex ">
+                                        <div className='border py-3 pl-2 w-50 '>Phone</div>
+                                        <div className='border py-3 pl-2 w-50 '>7852862759</div>
+                                    </div>
+                                    <div className="admin-table d-flex ">
+                                        <div className='border py-3 pl-2 w-50 '>Email</div>
+                                        <div className='border py-3 pl-2 w-50 '>admin@eshopper.com</div>
+                                    </div>
+                                    <div className="admin-table d-flex ">
+                                        <div className='border py-3 pl-2 w-50 '>Role</div>
+                                        <div className='border py-3 pl-2 w-50 '>Admin</div>
+                                    </div>
+                                    <div className="admin-table d-flex ">
+                                        <div className='border py-3 pl-2 w-50 '>Company Name</div>
+                                        <div className='border py-3 pl-2 w-50 '>R.A trades</div>
+                                    </div>
+
+
+                                    <div className="admin-table ">
+                                        <Link to="/" className='btn w-100 text-light bg-secondary mt-2'>Update</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
                 </div>
             </div>
-        </div>
-    </>
-  )
+
+        </>
+    )
 }
